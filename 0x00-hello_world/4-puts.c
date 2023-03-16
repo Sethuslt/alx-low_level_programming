d06e44b9ffdb83875602ccf018b93b226a8e0e1b
Sethuslt <temaswati06@gmail.com>
2023-03-16 16:31:25 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main fuction
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multiligual puzzle");
+	return (0);
+}
+

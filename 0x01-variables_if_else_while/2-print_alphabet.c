537254f715bd2de803551dee00c23b352305f511
Sethuslt <temaswati06@gmail.com>
2023-03-19 07:23:47 -0700

    1-last-digit.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - print alphabets in lower case
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char lc;
+	
+	for (lc = 'a'; lc <= 'z'; lc++);
+	{
+		putchar(lc);
+	}
+	putchcar('\n');
+
+	return (0);
+}

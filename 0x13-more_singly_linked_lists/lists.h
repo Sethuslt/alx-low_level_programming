086dc53411a2c2ccd785775a48e6a2510f94262b
Sethuslt <temaswati06@gmail.com>
2023-05-02 05:59:35 -0700

    list.h

---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,20 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+/**
+ * struct listint_s - Is the Singly Linked List.
+ * @n: Is The Interger
+ * Description: This Is The Singly List Node Strucure.
+ * @next: This Points To The Adjacent Node Structure.
+ *
+ *
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+size_t print_listint(const listint_t *h);
+
+#endif
